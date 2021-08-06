7d5b392e6e86a60b5c906c44b5b649c73ff92b0f
Francisco <franciscoliratorres@gmail.com>
2021-08-06 20:07:09 -0300

    add implementaçao de erros

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,19 @@
+import csv
+import os.path
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    document_type = path_to_file[-3:]
+    if document_type != "csv":
+        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
+    if os.path.exists(path_to_file) is False:
+        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
+    report_list = []
+    with open(path_to_file, mode="r") as file:
+        report_reader = csv.DictReader(file)
+        for row in report_reader:
+            report_list.append(row)
+    return report_list
+
+
+print(analyze_log("data/orders_1.csv"))
