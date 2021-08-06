4a67717d4f85647082029c52d8f801e4310b152c
Pablo Pessanha <pablopessanh@gmail.com>
2021-08-06 09:35:43 -0300

    requisitos obrigatorios

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,52 @@
+import csv
+from collections import Counter
+
+
+def get_csv_data(path):
+    if not path.endswith(".csv"):
+        raise FileNotFoundError(f"No such file or directory: '{path}'")
+
+    data = csv.DictReader(open(path), fieldnames=["name", "order", "day"])
+    return [row for row in data]
+
+
+def favorite_food(name, data):
+    customer_orders = [
+        customer["order"] for customer in data if customer["name"] == name
+    ]
+    return Counter(customer_orders).most_common(1)[0][0]
+
+
+def repeated_orders(name, meal, data):
+    customer_order = [
+        customer["order"] for customer in data if customer["name"] == name
+    ]
+    return Counter(customer_order).get(meal)
+
+
+def never_ordered(name, data):
+    orders = {customer["order"] for customer in data}
+    orders_per_customer = {
+        customer["order"] for customer in data if customer["name"] == name
+    }
+    return orders.symmetric_difference(orders_per_customer)
+
+
+def no_record(name, data):
+    order_days = {customer["day"] for customer in data}
+    days_customer = {
+        customer["day"] for customer in data if customer["name"] == name
+    }
+    return order_days.symmetric_difference(days_customer)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    data = get_csv_data(path_to_file)
+
+    fav = favorite_food("maria", data)
+    repeated = repeated_orders("arnaldo", "hamburguer", data)
+    non_ord = never_ordered("joao", data)
+    days = no_record("joao", data)
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(f"{fav}\n{repeated}\n{non_ord}\n{days}")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,22 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders_food = []
+        for order in self.orders:
+            if order[0] == costumer:
+                orders_food.append(order[1])
+        return Counter(orders_food).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
@@ -15,7 +25,13 @@ class TrackOrders:
         pass
 
     def get_busiest_day(self):
-        pass
+        days = []
+        for order in self.orders:
+            days.append(order[2])
+        return Counter(days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        for order in self.orders:
+            days.append(order[2])
+        return Counter(days).most_common()[-1][0]
