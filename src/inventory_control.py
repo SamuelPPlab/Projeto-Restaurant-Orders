1c88f3f074a7ffd94b00eb9557a870a4b636d55d
Vitor Rodrigues <vitor_rc@outlook.com>
2021-08-06 08:23:58 -0300

    inventory_control

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,27 +1,68 @@
 class InventoryControl:
-    INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho', 'tomate'],
-        'queijo-quente': ['pao', 'queijo', 'queijo'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'bauru': ['pao', 'queijo', 'presunto', 'tomate'],
-        'coxinha': ['massa', 'frango'],
-    }
-    MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
-    }
-
     def __init__(self):
-        pass
+        self.INGREDIENTS = {
+            'hamburguer': ['pao', 'carne', 'queijo'],
+            'pizza': ['massa', 'queijo', 'molho', 'tomate'],
+            'queijo-quente': ['pao', 'queijo', 'queijo'],
+            'misto-quente': ['pao', 'queijo', 'presunto'],
+            'bauru': ['pao', 'queijo', 'presunto', 'tomate'],
+            'coxinha': ['massa', 'frango'],
+        }
+        self.MINIMUM_INVENTORY = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+            'tomate': 50,
+        }
+
+        self.to_buy = {
+                'pao': 0,
+                'carne': 0,
+                'queijo': 0,
+                'molho': 0,
+                'presunto': 0,
+                'massa': 0,
+                'frango': 0,
+                'tomate': 0,
+            }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        available_ingredients = set()
+        for ingredient in self.INGREDIENTS[order]:
+            if self.MINIMUM_INVENTORY[ingredient] > self.to_buy[ingredient]:
+                available_ingredients.add(ingredient)
+            else:
+                return False
+
+        for available_ingredient in available_ingredients:
+            self.to_buy[available_ingredient] += 1
+
+        return True
 
     def get_quantities_to_buy(self):
-        pass
+        return self.to_buy
+
+    def get_available_dishes(self):
+        not_available_ingredients = set()
+        for ingredient in self.MINIMUM_INVENTORY.keys():
+            if (
+                self.MINIMUM_INVENTORY[ingredient] - self.to_buy[ingredient]
+                <= 0
+            ):
+                not_available_ingredients.add(ingredient)
+
+        available_products = set()
+        for product_name in self.INGREDIENTS.keys():
+            check = any(
+                ingredient in self.INGREDIENTS[product_name]
+                for ingredient in not_available_ingredients
+            )
+
+            print(check)
+            if not check:
+                available_products.add(product_name)
+        return available_products
