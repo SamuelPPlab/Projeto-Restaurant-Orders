d32658d2de04cc152cceb06c74147843bab2de9e
alvarovascon <alvarovascon2@gmail.com>
2021-08-07 17:32:00 -0300

    80%

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,26 @@
+from src.analyze_log import get_favorite
+from src.analyze_log import get_never_ordered
+from src.analyze_log import get_missing_days
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return get_favorite(self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return get_never_ordered(self.orders, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
-
+        return get_missing_days(self.orders, costumer)
     def get_busiest_day(self):
         pass
 
