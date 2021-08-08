bd5510e984de6cdcc95c7d68a86a01da78e009e0
kramer <kramerscs@gmail.com>
2021-08-08 19:12:32 -0300

    feat: req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,77 @@
+import csv
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
+    def get_orders(self):
+        with open('data/orders_1.csv', 'r') as logs:
+            reader = csv.reader(logs, delimiter=',')
+            orders = [item for item in reader]
+
+        return orders
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
+        orders = self.orders
+        count = {}
+        costumer_orders = [item[1] for item in orders if item[0] == costumer]
+        most_frequent = costumer_orders[0]
+
+        for order in costumer_orders:
+            if order not in count:
+                count[order] = costumer_orders.count(order)
+            if count[order] > count[most_frequent]:
+                most_frequent = order
+
+        return most_frequent
+
+    def get_order_frequency_per_costumer(self, costomer, order):
         pass
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
+    def get_never_ordered_per_costumer(self, custumer):
+        orders = self.orders
+        customer_plates = [item[1] for item in orders if item[0] == custumer]
+        never_ordered = [
+            item[1]
+            for item in orders if customer_plates.count(item[1]) == 0
+        ]
+
+        return set(never_ordered)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
 
     def get_busiest_day(self):
-        pass
+        orders = self.orders
+        count = {}
+        orders_by_day = [item[2] for item in orders]
+        most_frequent = orders_by_day[0]
+        for order in orders_by_day:
+            if order not in count:
+                count[order] = orders_by_day.count(order)
+            if count[order] > count[most_frequent]:
+                most_frequent = order
+
+        return most_frequent
 
     def get_least_busy_day(self):
-        pass
+        orders = self.orders
+        count = {}
+        orders_by_day = [item[2] for item in orders]
+
+        less_frequent = orders_by_day[0]
+
+        for order in orders_by_day:
+            if order not in count:
+                count[order] = orders_by_day.count(order)
+            if count[order] < count[less_frequent]:
+                less_frequent = order
+
+        return less_frequent
