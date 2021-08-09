edf1f9b50f1ea0f70dc71535440e31bdec5addd7
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-08-09 17:44:14 -0300

    Req2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -4,13 +4,28 @@ import collections
 
 def analyze_log(path_to_file):
     lista = []
+    lista2 = 0
+    pratosN = set()
+    pedidosN = set()
+    totalDias = set()
+    userDias = set()
     with open(path_to_file, newline="") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=",")
         for row in spamreader:
+            pedidosN.add(row[1])
+            totalDias.add(row[2])
             if row[0] == "maria":
                 lista.append(row[1])
+            if row[0] == "arnaldo" and row[1] == "hamburguer":
+                lista2 += 1
+            if row[0] == "joao":
+                pratosN.add(row[1])
+                userDias.add(row[2])
     # result2 = collections.Counter(lista).most_common()
-    result1 = collections.Counter(lista).most_common(1)[0][0]
+    result1 = f"{collections.Counter(lista).most_common(1)[0][0]}\n"
+    result1 += f"{lista2}\n"
+    result1 += f"{pedidosN.difference(pratosN)}\n"
+    result1 += f"{totalDias.difference(userDias)}\n"
     print(result1)
     # print("teste", result2)
 
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,51 @@
+import collections
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.pedidos)
+
+    def __init__(self):
+        self.pedidos = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.pedidos.append(
+            {"name": costumer, "order": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        result = []
+        for i in self.pedidos:
+            if i["name"] == costumer:
+                result.append(i["order"])
+        return collections.Counter(result).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        all_dish = set()
+        costumer_dish = set()
+        for pedido in self.pedidos:
+            all_dish.add(pedido["order"])
+            if pedido["name"] == costumer:
+                costumer_dish.add(pedido["order"])
+        return all_dish.difference(costumer_dish)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        all_days = set()
+        costumer_day = set()
+        for pedido in self.pedidos:
+            all_days.add(pedido["day"])
+            if pedido["name"] == costumer:
+                costumer_day.add(pedido["day"])
+        return all_days.difference(costumer_day)
 
     def get_busiest_day(self):
-        pass
+        days = []
+        for pedido in self.pedidos:
+            days.append(pedido["day"])
+        return collections.Counter(days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        for pedido in self.pedidos:
+            days.append(pedido["day"])
+        return collections.Counter(days).most_common()[-1][0]
