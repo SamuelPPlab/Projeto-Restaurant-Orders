f92b4e47bcee8ae8b8a21342d93a7e7d9555725b
Lfocosi <luccafocosi@gmail.com>
2021-08-09 18:05:56 -0300

    meu test local n funciona

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,63 @@
+import csv
+
+
+def leitor_arquivo(caminho):
+    with open(caminho) as arquivo_csv:
+        leitor_csv = csv.reader(arquivo_csv, delimiter=",")
+        pedidos = list(leitor_csv)
+    return pedidos
+
+
+def mais_pedido(nome, pedidos):
+    mais_pedido = ""
+    for pedido in pedidos:
+        if pedido[0] == nome:
+            mais_pedido = pedido[1]
+            break
+    return mais_pedido
+
+
+def qunatidade(nome, prato, pedidos):
+    quantidades = set()
+    for pedido in pedidos:
+        if pedido[0] == nome and pedido[1] == prato:
+            quantidades.add(pedido[1])
+    return len(quantidades)
+
+
+def nunca_pedidos(nome, pedidos):
+    pedidos_pessoa = set()
+    todos_pedidos = set()
+    for pedido in pedidos:
+        todos_pedidos.add(pedido[1])
+    for cliente in pedidos:
+        if cliente[0] == nome:
+            pedidos_pessoa.add(cliente[1])
+    return todos_pedidos.difference(pedidos_pessoa)
+
+
+def dias_sem_pedir(nome, pedidos):
+    dias_com_pedidos = set()
+    dias = set()
+    for pedido in pedidos:
+        dias.add(pedido[2])
+    for pedido in pedidos:
+        if pedido[0] == nome:
+            dias_com_pedidos.add(dia[2])
+    return dias.difference(dias_com_pedidos)
+
+
 def analyze_log(path_to_file):
-    print("comit")
-    raise NotImplementedError
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
+    orders = leitor_arquivo(path_to_file)
+    with open("data/mkt_campaign.txt", "w") as analyze_file:
+        analyze_file.write(f"{mais_pedido("maria", orders)}\n")
+        analyze_file.write(
+            f"{qunatidade("arnaldo","hamburguer", orders)}\n"
+        )
+        analyze_file.write(
+            f"{nunca_pedidos("joao", orders)}\n"
+        )
+        analyze_file.write(f"{dias_sem_pedir("joao", orders)}\n")
+    
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,52 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        lista = []
+        for pedido in self.orders:
+            if pedido[0] == costumer:
+                lista.append(pedido[1])
+        resultado = Counter(lista)
+        return resultado.most_common()[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        todos_pedidos = set()
+        pedidos_cliente = set()
+        for pedido in self.orders:
+            todos_pedidos.add(pedido[1])
+            if pedido[0] == costumer:
+                pedidos_cliente.add(pedido[1])
+        return todos_pedidos.difference(pedidos_cliente)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        dias = set()
+        dias_com_pedidos = set()
+        for pedido in self.orders:
+            dias.add(pedido[2])
+            if pedido[0] == costumer:
+                dias_com_pedidos.add(pedido[2])
+        return dias.difference(dias_com_pedidos)
 
     def get_busiest_day(self):
-        pass
+        lista = []
+        for pedido in self.orders:
+            lista.append(pedido[2])
+        resultado = Counter(lista)
+        return resultado.most_common()[0][0]
 
     def get_least_busy_day(self):
-        pass
+        lista = []
+        for pedido in self.orders:
+            lista.append(pedido[2])
+        resultado = Counter(lista)
+        return resultado.most_common()[-1][0]
