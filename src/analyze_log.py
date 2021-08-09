2d7fe0d2533d55d2dd3aee6a743728dfcde6546a
danwhat <daanwhat@gmail.com>
2021-08-09 15:39:40 -0300

    pretty

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def analyze_log(path_to_file):
     with open(path_to_file) as file:
         file = csv.reader(file, delimiter=",", quotechar='"')
@@ -12,27 +13,32 @@ def analyze_log(path_to_file):
         joao_didt_come = set()
 
         for line in file:
-          # joao
-          if line[0] == 'joao': joao_asked.add(line[1])
-          else: joao_not_asked.add(line[1])
-          # joao came
-          if line[0] == 'joao': joao_came.add(line[2])
-          else: joao_didt_come.add(line[2])
-          # maria
-          if (line[0] == "maria"):
-            if (line[1] in maria.keys()):
-              maria[line[1]] += 1
+            # joao
+            if line[0] == "joao":
+                joao_asked.add(line[1])
             else:
-              maria[line[1]] = 1
-          # arnaldo_hamburger_count
-          if (line[0] == "arnaldo" and line[1] == "hamburguer"): arnaldo_hamburguer_count += 1
-        
+                joao_not_asked.add(line[1])
+            # joao came
+            if line[0] == "joao":
+                joao_came.add(line[2])
+            else:
+                joao_didt_come.add(line[2])
+            # maria
+            if line[0] == "maria":
+                if line[1] in maria.keys():
+                    maria[line[1]] += 1
+                else:
+                    maria[line[1]] = 1
+            # arnaldo_hamburger_count
+            if line[0] == "arnaldo" and line[1] == "hamburguer":
+                arnaldo_hamburguer_count += 1
+
         # get maria_favorite
         for key in maria.keys():
-          if (maria_favorite == ""):
-            maria_favorite = key
-          if (maria[key] > maria[maria_favorite]):
-            maria_favorite = key
+            if maria_favorite == "":
+                maria_favorite = key
+            if maria[key] > maria[maria_favorite]:
+                maria_favorite = key
 
         stringao = f"""{str(maria_favorite)}
 {str(arnaldo_hamburguer_count)}
@@ -43,5 +49,6 @@ def analyze_log(path_to_file):
         f.write(stringao)
         f.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     analyze_log("data/orders_1.csv")
