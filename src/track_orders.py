9f73e993a4a38f4f525a7b0ef192f23a6646cb63
PedroMarquesFr2 <pedromarques1011@gmail.com>
2021-08-09 10:38:51 -0300

    req2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,48 @@
+# from analyze_log import AnalyzeDataByPerson
+
+
 class TrackOrders:
+    def __init__(self):
+        self.stats = {}
+        self.days = {}
+        self.orders = set()
+
     def __len__(self):
-        pass
+        return len(self.stats)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer not in self.stats:
+            self.stats[costumer] = {"orders": {order: 1}, "days": set()}
+        if order not in self.stats[costumer]["orders"].keys():
+            self.stats[costumer]["orders"][order] = 1
+        else:
+            self.stats[costumer]["orders"][order] += 1
+        if day not in self.days:
+            self.days[day] = 1
+        else:
+            self.days[day] += 1
+        self.orders.add(order)
+        self.stats[costumer]["days"].add(day)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        for order in self.stats[costumer]["orders"].keys():
+            if self.stats[costumer]["orders"][order] == max(
+                self.stats[costumer]["orders"].values()
+            ):
+                return order
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.orders.difference(self.stats[costumer]["orders"].keys())
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return set(self.days.keys()).difference(self.stats[costumer]["days"])
 
     def get_busiest_day(self):
-        pass
+        for day in self.days.keys():
+            if self.days[day] == max(self.days.values()):
+                return day
 
     def get_least_busy_day(self):
-        pass
-
-    def get_order_frequency_per_costumer(self, name, order):
-        pass
+        for day in self.days.keys():
+            if self.days[day] == min(self.days.values()):
+                return day
