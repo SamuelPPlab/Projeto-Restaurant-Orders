0a7fc0383e292981cb94f58fbe588386b5d20ee2
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-08-09 15:56:42 -0300

    implementação método add_new_order

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,9 +1,22 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = {}
+
     def __len__(self):
         pass
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer in self.orders:
+            if order in self.orders[costumer]["dishes"]:
+                self.orders[costumer]["dishes"][order] += 1
+            else:
+                self.orders[costumer]["dishes"][order] = 1
+            if day in self.orders[costumer]["days"]:
+                self.orders[costumer]["days"][day] += 1
+            else:
+                self.orders[costumer]["days"][day] = 1
+        else:
+            self.orders[costumer] = {"dishes": {order: 1}, "days": {day: 1}}
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
