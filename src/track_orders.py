7449871988b13603778f2f098266b82b5219af61
nevesfreire <nando.neves@gmail.com>
2021-08-09 17:45:31 -0300

    first

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,51 @@
+import collections
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.pedidos)
+
+    def __init__(self):
+        self.pedidos = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.pedidos.append(
+            {"name": costumer, "order": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        order_list = []
+        for item in self.pedidos:
+            if item["name"] == costumer:
+                order_list.append(item.order)
+        return collections.Counter(order_list).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        all_dishes = set()
+        costumer_dishes = set()
+        for pedido in self.pedidos:
+            all_dishes.add(pedido["order"])
+            if pedido["name"] == costumer:
+                costumer_dishes.add(pedido["order"])
+        return all_dishes.difference(costumer_dishes)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        all_days = set()
+        costumer_day = set()
+        for pedido in self.pedidos:
+            all_days.add(pedido["day"])
+            if pedido["name"] == costumer:
+                costumer_day.add(pedido["day"])
+            return all_days.difference(costumer_day)
 
     def get_busiest_day(self):
-        pass
+        all_days = []
+        for pedido in self.pedidos:
+            all_days.append(pedido["day"])
+        return collections.Counter(all_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        all_days = []
+        for pedido in self.pedidos:
+            all_days.append(pedido["day"])
+        return collections.Counter(all_days).most_common()[-1][0]
