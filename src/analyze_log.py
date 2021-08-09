0b26b1eb035272dd4baac465c3e30003ff2b915e
nevesfreire <nando.neves@gmail.com>
2021-08-09 18:22:47 -0300

    second

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,32 @@
+import collections
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    m_orders = []
+    orders = set()
+    days = set()
+    arnaldo_counter = 0
+    joao_orders = set()
+    joao_days = set()
+    answers = ""
+
+    with open(path_to_file) as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=",")
+        for line in spamreader:
+            orders.add(line[1])
+            days.add(line[2])
+            if line[0] == "maria":
+                m_orders.append(line[1])
+            if line[0] == "arnaldo" and line[1] == "hamburguer":
+                arnaldo_counter += 1
+            if line[0] == "joao":
+                joao_orders.add(line[1])
+                joao_days.add(line[2])
+        answers += f"{collections.Counter(m_orders).most_common(1)[0][0]}\n"
+        answers += f"{arnaldo_counter}\n"
+        answers += f"{orders.difference(joao_orders)}\n"
+        answers += f"{days.difference(joao_days)}"
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(answers)
