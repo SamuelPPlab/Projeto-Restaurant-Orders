136f3bbb42c594da1e53e898f0cccc7bd9622edb
Vanessa Naára Oliveira <vanessanaara@gmail.com>
2021-08-09 14:29:29 -0300

    Iniciando req 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,17 @@
+from csv import DictReader
+import os
+
+
+def read_data(path):
+    if not path.endswith(".csv") or not os.path.isfile(path):
+        raise FileNotFoundError(f"No such file or directory: '{path}'")
+    with open(path, "r") as csv_file:
+        fieldnames = ["client", "order", "day"]
+        reader = DictReader(csv_file, fieldnames=fieldnames)
+        data = list(row for row in reader)
+    return data
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    data = read_data(path_to_file)
+    return data
