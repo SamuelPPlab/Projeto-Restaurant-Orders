de3f4b8ec3cf44fb236954ef6d35e3e24aecfa1d
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-08-09 16:48:12 -0300

    Veremos hein

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,67 @@
-class InventoryControl:
-    INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
-    }
-    MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
-    }
+from src.track_orders import TrackOrders
 
+class InventoryControl:
+    orders = TrackOrders()
     def __init__(self):
-        pass
+
+        self.ingredients = {
+            'hamburguer': ['pao', 'carne', 'queijo'],
+            'pizza': ['massa', 'queijo', 'molho'],
+            'misto-quente': ['pao', 'queijo', 'presunto'],
+            'coxinha': ['massa', 'frango'],
+        }
+        self.minimum_inventory = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
+
+        self.toBuy = {
+            "pao": 0,
+            "carne": 0,
+            "queijo": 0,
+            "molho": 0,
+            "presunto": 0,
+            "massa": 0,
+            "frango": 0,
+        }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        index = 0
+        ingredients = self.ingredients(order)
+        while (
+            index < len(ingredients)
+            and self.toBuy[ingredients[index]]
+            < self.minimum_inventory[ingredients[index]]
+        ):
+            index += 1
+        
+        if index == len(ingredients):
+            for ingredient in self.ingredients[order]:
+                self.toBuy[ingredient] = self.toBuy[ingredient] + 1
+        else:
+            return False
 
     def get_quantities_to_buy(self):
-        pass
+        return self.toBuy
+
+    def get_available_dishes(self):
+        dishes = self.ingredients.keys()
+        available_dishes = []
+        for dish in dishes:
+            ingredients = self.ingredients[dish]
+            index = 0
+            while (
+                index < len(ingredients)
+                and self.toBuy[ingredients[index]]
+            ):
+                index += 1
+            
+            if index == len(ingredients):
+                available_dishes = [*available_dishes, dish]
+            
+        return set(available_dishes)
