03cb8223d08678679e8ba211f98e5d3013f61d8e
Éric Vinícius <ericcobain3@gmail.com>
2021-08-10 15:42:35 -0300

    formaturaaa

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,51 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import collections
+import csv
+
+orders = set()
+days = set()
+maria_orders = []
+arnaldo_frequency = 0
+joao_dishes = set()
+joao_days = set()
+
+
+def analyze_log(path):
+    expect_text = "No such file or directory: " "'data/orders_1.txt'"
+    if path.endswith(".csv"):
+        with open(path) as file:
+
+            read_file = csv.reader(file, delimiter=",")
+            analyze(read_file)
+
+    else:
+        raise FileNotFoundError(expect_text)
+
+
+def analyze(file):
+    arnaldo_frequency = 0
+
+    for line in file:
+
+        if(line[0] == "maria"):
+            maria_orders.append(line[1])
+        if(line[0] == "arnaldo" and line[1] == "hamburguer"):
+            arnaldo_frequency += 1
+        if(line[0] == "joao"):
+            joao_dishes.add(line[1])
+            joao_days.add(line[2])
+
+        orders.add(line[1])
+        days.add(line[2])
+
+    most_common_dishe = collections.Counter(maria_orders).most_common(1)[0][0]
+    joao_never_ask = orders.difference(joao_dishes)
+    joao_never_go = days.difference(joao_days)
+
+    final_file = f"{most_common_dishe}\n"
+    final_file += f"{arnaldo_frequency}\n"
+    final_file += f"{joao_never_ask}\n"
+    final_file += f"{joao_never_go}\n"
+
+    f = open("data/mkt_campaign.txt", "w")
+    f.write(final_file)
+    f.close()
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,54 @@
+import collections
+
+c = collections.Counter()
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.orders)
+
+    def __init__(self):
+        self.orders = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"name": costumer, "order": order, "day": day})
+        return self.orders
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        most_ordered = []
+        for order in self.orders:
+            if order["name"] == costumer:
+                most_ordered.append(order["order"])
+        return collections.Counter(most_ordered).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        dishes = set()
+        dishe_ordered = set()
+        for order in self.orders:
+            if(order["name"] == costumer):
+                dishe_ordered.add(order["order"])
+            dishes.add(order["order"])
+        never_ordered = dishes.difference(dishe_ordered)
+        return never_ordered
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        days_visited = set()
+        for order in self.orders:
+            if(order["name"] == costumer):
+                days_visited.add(order["day"])
+            days.add(order["day"])
+        days_never_visited = days.difference(days_visited)
+        return days_never_visited
 
     def get_busiest_day(self):
-        pass
+        all_days = []
+        for order in self.orders:
+            all_days.append(order["day"])
+        return collections.Counter(all_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        all_days = []
+        for order in self.orders:
+            all_days.append(order["day"])
+        return collections.Counter(all_days).most_common()[-1][0]
