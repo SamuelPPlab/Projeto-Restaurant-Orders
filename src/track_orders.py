4d5f7d71a6240f8d8389f738bb6c88bfa7c2b4de
Filipi Firmino <contatoic3web@gmail.com>
2021-08-11 19:27:11 -0300

    challenge 2 complete

--- src/track_orders.py
+++ src/track_orders.py
@@ -23,17 +23,33 @@ class TrackOrders:
             if item['name'] == costumer:
                 dish_ordered.append(item['order'])
             ordered.append(item['order'])
-            never_ordered = {
-                item for item in ordered
-                if item not in dish_ordered
-                }
-            return never_ordered
+        never_ordered = {
+            item for item in ordered
+            if item not in dish_ordered
+        }
+        return never_ordered
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = []
+        all_days = []
+        for item in self.orders:
+            if item['name'] == costumer:
+                days.append(item['day'])
+            all_days.append(item['day'])
+        never_visited = {
+            item for item in sorted(set(all_days))
+            if item not in sorted(set(days))
+        }
+        return never_visited
 
     def get_busiest_day(self):
-        pass
+        days = []
+        for item in self.orders:
+            days.append(item['day'])
+        return max(set(days), key=days.count)
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        for item in self.orders:
+            days.append(item['day'])
+        return min(set(days), key=days.count)
