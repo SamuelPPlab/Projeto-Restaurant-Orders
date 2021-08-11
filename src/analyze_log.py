a96b16f2157c1e0d728bf0ca4aa435a49329fb7c
Serudo <andreserudo@gmail.com>
2021-08-10 22:51:11 -0400

    andreserudo - restaurant-orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,80 @@
+import csv
+
+
+def read_file_to_dictionary(path_to_file):
+    file_type = path_to_file.split(".")[1]
+    if file_type == "csv":
+        columns_names = ['client_name', 'order', 'days_of_week']
+        with open(path_to_file, newline="") as csvfile:
+            reader = list(csv.DictReader(csvfile, columns_names))
+    else:
+        raise ValueError(f'No such file or directory: {path_to_file}')
+    return reader
+
+
+def most_wanted_order(orders):
+    order_count = {}
+    most_frequent = orders[0]
+
+    for order in orders:
+        if order not in order_count:
+            order_count[order] = 1
+        else:
+            order_count[order] += 1
+        if order_count[order] > order_count[most_frequent]:
+            most_frequent = order
+    return most_frequent
+
+
+def count_orders_in_order(orders, order_to_match):
+    count_order = 0
+    for order in orders:
+        if order_to_match == order:
+            count_order += 1
+
+    return count_order
+
+
+def order_never_ordered_from_menu(joao_orders):
+    menu = set(['hamburguer', 'pizza', 'coxinha', 'misto-quente'])
+    orders = set(joao_orders)
+
+    return menu.difference(orders)
+
+
+def days_with_no_order(days_with_order):
+    days_of_week = set(['segunda-feira', 'terça-feira', 'quarta-feira',
+                        'quinta-feira', 'sexta-feira', 'sábado', 'domingo'])
+    days = set(days_with_order)
+
+    return days_of_week.difference(days)
+
+
+def write_file(filename, text):
+    with open(filename, "a") as txtfile:
+        txtfile.writelines(str(text) + "\n")
+        txtfile.close()
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    orders_history = read_file_to_dictionary(path_to_file)
+    filename = 'data/mkt_campaign.txt'
+    maria_orders = []
+    arnaldo_orders = []
+    joao_orders = []
+    joao_weekdays = []
+
+    for orders in orders_history:
+        if(orders['client_name'] == 'maria'):
+            maria_orders.append(orders['order'])
+        elif(orders['client_name'] == 'arnaldo'):
+            arnaldo_orders.append(orders['order'])
+        elif(orders['client_name'] == 'joao'):
+            joao_orders.append(orders['order'])
+            joao_weekdays.append(orders['days_of_week'])
+
+    write_file(filename, most_wanted_order(maria_orders))
+    write_file(filename, count_orders_in_order(arnaldo_orders, 'hamburguer'))
+    write_file(filename, order_never_ordered_from_menu(joao_orders))
+    write_file(filename, days_with_no_order(joao_weekdays))
+    # raise NotImplementedError
