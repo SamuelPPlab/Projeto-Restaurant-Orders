53af345345ecf43c874d8bd998bc638d7f5ec724
Fernando Antunes <fernando.antunes1@gmail.com>
2021-08-12 09:52:02 -0300

    Requisito 2 - item 2.4

--- src/track_orders.py
+++ src/track_orders.py
@@ -9,20 +9,20 @@ class TrackOrders:
         self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        food_requested = {}
-        for row in self.orders:
-            if row[0] == costumer:
-                if row[1] in food_requested:
-                    food_requested[row[1]] += 1
-                else:
-                    food_requested[row[1]] = 1
+        food_requested = self.food_requested(costumer)
         return max(food_requested, key=food_requested.get)
 
     def get_dish_quantity_per_costumer(self, costumer, order):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        food = self.food_requested(costumer)
+        menu = self.get_menu()
+        food_never_requested = set()
+        for item in menu:
+            if item not in food:
+                food_never_requested.add(item)
+        return food_never_requested
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
@@ -32,3 +32,20 @@ class TrackOrders:
 
     def get_least_busy_day(self):
         pass
+
+    def food_requested(self, costumer):
+        food_requested = {}
+        for row in self.orders:
+            if row[0] == costumer:
+                if row[1] in food_requested:
+                    food_requested[row[1]] += 1
+                else:
+                    food_requested[row[1]] = 1
+        return food_requested
+
+    def get_menu(self):
+        menu = set()
+        for row in self.orders:
+            if row[1] not in menu:
+                menu.add(row[1])
+        return menu
