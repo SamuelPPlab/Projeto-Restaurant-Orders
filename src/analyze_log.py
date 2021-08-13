d3403c28fb1ba660e9895086a45cb1178762988f
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-08-13 07:14:46 -0300

    requisito 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -5,8 +5,8 @@ def create_log_dict(file_list):
 
     for order in file_list:
         order_list = order.split(',')
-        menu = menu.union({ order_list[1] })
-        week_days = week_days.union({ order_list[2] })
+        menu = menu.union({order_list[1]})
+        week_days = week_days.union({order_list[2]})
 
         if order_list[0] not in log_dict:
             log_dict[order_list[0]] = {
@@ -17,12 +17,21 @@ def create_log_dict(file_list):
             }
         elif order_list[1] in log_dict[order_list[0]]['dishes']:
             log_dict[order_list[0]]['dishes'][order_list[1]] += 1
-            log_dict[order_list[0]]['visit_days'] = log_dict[order_list[0]]['visit_days'].union(set([order_list[2]]))
+            log_dict[order_list[0]]['visit_days'] = (
+                log_dict[order_list[0]]['visit_days'].union(
+                    set([order_list[2]])
+                )
+            )
         else:
             log_dict[order_list[0]]['dishes'][order_list[1]] = 1
-            log_dict[order_list[0]]['visit_days'] = log_dict[order_list[0]]['visit_days'].union(set([order_list[2]]))
+            log_dict[order_list[0]]['visit_days'] = (
+                log_dict[order_list[0]]['visit_days'].union(
+                    set([order_list[2]])
+                )
+            )
 
     return [log_dict, menu, week_days]
+
 
 def get_most_required_dish(customer_name, log):
     biggest_number = 0
@@ -35,6 +44,7 @@ def get_most_required_dish(customer_name, log):
 
     return dish_name
 
+
 def analyze_log(path_to_file):
     log_dict = {}
     response_list = []
@@ -46,15 +56,16 @@ def analyze_log(path_to_file):
         log_dict = create_log_dict(file_list)[0]
         menu = create_log_dict(file_list)[1]
         week_days = create_log_dict(file_list)[2]
-    
 
     response_list.append(get_most_required_dish('maria', log_dict))
     response_list.append(log_dict['arnaldo']['dishes']['hamburguer'])
-    response_list.append(menu.difference(set(log_dict['joao']['dishes'].keys())))
-    response_list.append(week_days.difference(log_dict['joao']['visit_days']))
-    
+    response_list.append(menu.difference(
+        set(log_dict['joao']['dishes'].keys())
+    ))
+    response_list.append(week_days.difference(
+        log_dict['joao']['visit_days']
+    ))
+
     with open('data/mkt_campaign.txt', 'w') as file_data:
         for response in response_list:
             file_data.write(f'{response}\n')
-
-analyze_log('data/orders_1.csv')
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,81 @@
 class TrackOrders:
+    orders = {}
+    menu = set()
+    days = set()
+    visited_days = {}
+    size = 0
+
     def __len__(self):
-        pass
+        return self.size
+
+    def add_visited_day(self, day):
+        if day not in self.visited_days:
+            self.visited_days[day] = 1
+        else:
+            self.visited_days[day] += 1
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.size += 1
+
+        if costumer not in self.orders:
+            self.orders[costumer] = {
+                'dishes': {
+                    order: 1
+                },
+                'day': {day}
+            }
+        elif order in self.orders[costumer]['dishes']:
+            self.orders[costumer]['dishes'][order] += 1
+            self.orders[costumer]['day'] = (
+                self.orders[costumer]['day'].union({day})
+            )
+        else:
+            self.orders[costumer]['dishes'][order] = 1
+            self.orders[costumer]['day'] = (
+                self.orders[costumer]['day'].union({day})
+            )
+
+        self.add_visited_day(day)
+        self.menu = self.menu.union({order})
+        self.days = self.days.union({day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        dish = ''
+        bigger_number = 0
+
+        for order_name, quantity in self.orders[costumer]['dishes'].items():
+            if quantity > bigger_number:
+                dish = order_name
+                bigger_number = quantity
+
+        return dish
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.menu.difference(
+            set(self.orders[costumer]['dishes'].keys())
+        )
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return self.days.difference(self.orders[costumer]['day'])
 
     def get_busiest_day(self):
-        pass
+        busiest_day = ''
+        biggest_visited_number = 0
+
+        for day, visits in self.visited_days.items():
+            if visits > biggest_visited_number:
+                busiest_day = day
+                biggest_visited_number = visits
+
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        least_busy_day = list(self.visited_days.keys())[0]
+        least_visited_number = list(self.visited_days.values())[0]
+
+        for day, visits in self.visited_days.items():
+            if visits < least_visited_number:
+                least_busy_day = day
+                least_visited_number = visits
+
+        return least_busy_day
