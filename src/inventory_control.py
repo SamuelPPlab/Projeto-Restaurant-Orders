d744d3b63acf0b66d53adbc11b95478bf72317f5
jr3564 <rodolfojr94@gmail.com>
2021-08-14 19:50:20 -0300

    Requisito 2

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,30 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.__inventory = self.MINIMUM_INVENTORY
+        self.__ingredients = {
+            key: set(ingredients)
+            for key, ingredients in self.INGREDIENTS.items()
+        }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        for ingredient in self.__ingredients[order]:
+            self.__inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.__inventory
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,93 @@
+from src.utils.most_request_food import most_request_food
+from src.utils.best_counter import count
+
+
 class TrackOrders:
+    def __init__(self):
+        self.__orders_by_costumer = dict()
+        self.__len = 0
+        self.__food_menu = set()
+        self.__opening_days = set()
+
     def __len__(self):
-        pass
+        return self.__len
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer in self.__orders_by_costumer:
+            self.__orders_by_costumer[costumer].append((order, day))
+        else:
+            self.__orders_by_costumer[costumer] = [(order, day)]
+
+        self.__len += 1
+        self.__food_menu.add(order)
+        self.__opening_days.add(day)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return most_request_food(self.__orders_by_costumer[costumer])
+
+    def get_dish_quantity_per_costumer(self, costumer, order):
+        return count(
+            lambda item: True if order in item else False,
+            self.__orders_by_costumer[costumer],
+        )
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        client_foods = {
+            food for food, _ in self.__orders_by_costumer[costumer]
+        }
+        return self.__food_menu.difference(client_foods)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        client_went_to_the_restaurant = {
+            day for _, day in self.__orders_by_costumer[costumer]
+        }
+        return self.__opening_days.difference(client_went_to_the_restaurant)
+
+    @property
+    def customers_per_day(self):
+        days_for_quantities = dict()
+        for client in self.__orders_by_costumer:
+            for _, day in client:
+                if day in days_for_quantities:
+                    days_for_quantities[day] += 1
+                else:
+                    days_for_quantities[day] = 1
+        return days_for_quantities
 
     def get_busiest_day(self):
-        pass
+        # return max(self.customers_per_day)
+        days_for_quantities = dict()
+        busiest_day = None
+        for client in self.__orders_by_costumer.values():
+            for _, day in client:
+                if day in days_for_quantities:
+                    days_for_quantities[day] += 1
+                else:
+                    days_for_quantities[day] = 1
+
+                if (
+                    not busiest_day
+                    or days_for_quantities[day]
+                    > days_for_quantities[busiest_day]
+                ):
+                    busiest_day = day
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        # return min(self.customers_per_day)
+        days_for_quantities = dict()
+        least_busy_day = None
+        for client in self.__orders_by_costumer.values():
+            for _, day in client:
+                if day in days_for_quantities:
+                    days_for_quantities[day] += 1
+                else:
+                    days_for_quantities[day] = 1
+
+                if (
+                    not least_busy_day
+                    or days_for_quantities[day]
+                    < days_for_quantities[least_busy_day]
+                ):
+                    least_busy_day = day
+        return least_busy_day
