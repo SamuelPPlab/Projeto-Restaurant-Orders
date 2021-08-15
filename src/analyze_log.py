c949086d797257a60cd7e8b90e80b2d5d9698968
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-15 17:46:58 -0300

    requisito 02-v1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -119,7 +119,7 @@ def what_days_client_never_went_to_the_cafeteria(file, name_client):
 
 
 def analyze_log(path_to_file):
-    # raise NotImplementedError
+    raise NotImplementedError
     file = get_file_csv(path_to_file)
 
     food = get_most_requested_dish_by_name(file, "maria")
@@ -135,6 +135,3 @@ def analyze_log(path_to_file):
 
     days = what_days_client_never_went_to_the_cafeteria(file, "joao")
     save_text(days)
-
-
-analyze_log("data/orders_1.csv")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,21 @@
+from collections import Counter
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return 0
 
     def add_new_order(self, costumer, order, day):
-        pass
+        current_order = [costumer, order, day]
+        self.order_list.append(current_order)
 
+    # referência: https://stackoverflow.com/questions/3594514/how-to-find-most-common-elements-of-a-list/44481414
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        list_by_costumer = []
+        for item in self.order_list:
+            if costumer in item:
+                new_list.append(item)
+        return Counter(list_by_costumer).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
