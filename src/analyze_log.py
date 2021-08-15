55ce95f75cdca235cc6f926a97754877a7fc59a8
Strongreen <strongreen@live.com>
2021-08-15 13:35:11 -0300

    corrigindo flake8

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,17 +1,20 @@
 import csv
 
+
 def readCsv(path):
     log = []
-    with open( path, 'r',) as file:
-        reader = csv.reader(file, delimiter = '\n')
+    with open(path, 'r',) as file:
+        reader = csv.reader(file, delimiter='\n')
         for line in reader:
             log.append(line)
     return log
 
+
 def writeTxt(path, conteudo):
-    with open( path, 'w',) as file:
+    with open(path, 'w',) as file:
         for line in conteudo:
             file.writelines(line)
+
 
 def produtos(log):
     all_products = set()
@@ -19,6 +22,7 @@ def produtos(log):
         cliente = pedido[0].split(',')
         all_products.add(cliente[1])
     return all_products
+
 
 def diasSemana(log):
     semana = set()
@@ -33,7 +37,8 @@ def findClient(log, nomeCliente):
         cliente = pedido[0].split(',')
         if(cliente[0] == nomeCliente):
             return cliente[1]
-    
+
+
 def findFood(log, nomeCliente, food):
     count = 0
     for pedido in log:
@@ -43,6 +48,7 @@ def findFood(log, nomeCliente, food):
                 count += 1
     return count
 
+
 def foodNuncaPedido(log, nomeCliente, produtos):
     produto = set(produtos)
     for pedido in log:
@@ -50,7 +56,8 @@ def foodNuncaPedido(log, nomeCliente, produtos):
         if(cliente[0] == nomeCliente):
             produto.discard(cliente[1])
     return produto
-  
+
+
 def diasNuncaLanchonete(log, nomeCliente, dias_semana):
     produto = set(dias_semana)
     for pedido in log:
@@ -58,18 +65,16 @@ def diasNuncaLanchonete(log, nomeCliente, dias_semana):
         if(cliente[0] == nomeCliente):
             produto.discard(cliente[2])
     return produto
-        
+
+
 def analyze_log(path_to_file):
     log = readCsv(path_to_file)
     all_produtos = produtos(log)
     dias_semana = diasSemana(log)
     writeTxt('data/mkt_campaign.txt',
-        [
-        f"{findClient(log, 'maria')}\n",
-        f"{findFood(log, 'arnaldo', 'hamburguer')}\n",
-        f"{foodNuncaPedido(log, 'joao', all_produtos)}\n",
-        f"{diasNuncaLanchonete(log, 'joao', dias_semana)}\n",
-        ])
-
-  
-        
+             [
+                 f"{findClient(log, 'maria')}\n",
+                 f"{findFood(log, 'arnaldo', 'hamburguer')}\n",
+                 f"{foodNuncaPedido(log, 'joao', all_produtos)}\n",
+                 f"{diasNuncaLanchonete(log, 'joao', dias_semana)}\n",
+             ])
