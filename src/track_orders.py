6c4b649682f375d8e95ea7f230c12185eae8b964
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-08-15 14:50:40 -0300

    req 2.

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,49 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
+
+    def get_orders_by_customer(self, customer):
+        return [
+            order["order"] for order in self.orders
+            if order["name"] == customer
+        ]
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"name": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        result = self.get_orders_by_customer(costumer)
+        return max(set(result), key=result.count)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        result = set(self.get_orders_by_customer(costumer))
+        return {order["order"] for order in self.orders}.difference(result)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        result = [
+            order["day"] for order in self.orders if order["name"] == costumer
+        ]
+        return {order["day"] for order in self.orders}.difference(result)
 
     def get_busiest_day(self):
-        pass
+        days = dict()
+        for order in self.orders:
+            if order["day"] not in days:
+                days[order["day"]] = 1
+            else:
+                days[order["day"]] += 1
+
+        return max(days, key=days.get)
 
     def get_least_busy_day(self):
-        pass
+        days = dict()
+        for order in self.orders:
+            if order["day"] not in days:
+                days[order["day"]] = 1
+            else:
+                days[order["day"]] += 1
+
+        return min(days, key=days.get)
