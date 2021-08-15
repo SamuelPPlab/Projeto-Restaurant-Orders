cdae37979c091db4c87c2797d2414e6d0bfc2538
Camila Valentim <camilaifpb@hotmail.com>
2021-08-15 10:38:54 -0300

    requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -11,9 +11,8 @@ def request_maria(file_dict):
     ]
     return Counter(pedidos_maria).most_common(1)[0][0]
 
+
 # Quantas vezes 'arnaldo' pediu 'hamburguer'?
-
-
 def request_arnaldo(file_dict):
     pedidos_arnaldo = [
         pedido["pedido"]
@@ -22,6 +21,37 @@ def request_arnaldo(file_dict):
         # referencia Melissa-gomes
     ].count("hamburguer")
     return pedidos_arnaldo
+
+
+# Quais pratos 'joao' nunca pediu?
+def not_request_joao(file_dict):
+    total_pedidos = set([
+        pedido["pedido"]
+        for pedido in file_dict
+    ])
+    pedidos_joao = set([
+        pedido["pedido"]
+        for pedido in file_dict
+        if pedido['cliente'] == 'joao'
+
+    ])
+
+    return total_pedidos.difference(pedidos_joao)
+
+
+def not_joao_lanchonete(file_dict):
+    total_dias = set([
+        dia["dia"]
+        for dia in file_dict
+    ])
+    dias_not_joao = set([
+        dia["dia"]
+        for dia in file_dict
+        if dia['cliente'] == "joao"
+
+    ])
+
+    return total_dias.difference(dias_not_joao)
 
 
 def analyze_log(path_to_file):
@@ -35,11 +65,16 @@ def analyze_log(path_to_file):
         ]
         request_maria(file_dict)
         request_arnaldo(file_dict)
+        not_request_joao(file_dict)
+        not_joao_lanchonete(file_dict)
 
     with open("data/mkt_campaign.txt", mode="w") as f:
         f.writelines(
             f"{request_maria(file_dict)}\n"
             f"{request_arnaldo(file_dict)}\n"
+            f"{not_request_joao(file_dict)}\n"
+            f"{not_joao_lanchonete(file_dict)}\n"
+
         )
 
 # if __name__ == "__main__":
