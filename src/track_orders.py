d161501158331c71fc6d0c36d052ebddbbe3e181
Strongreen <strongreen@live.com>
2021-08-15 17:43:41 -0300

    requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,49 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.semana = set()
+        self.produtos = set()
+        self.pedidos = []
+
     def __len__(self):
-        pass
+        return len(self.pedidos)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.produtos.add(order)
+        self.semana.add(day)
+        self.pedidos.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        all_pedido = []
+        for pedido in self.pedidos:
+            if pedido[0] == costumer:
+                all_pedido.append(pedido[1])
+        return Counter(all_pedido).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        produto = set(self.produtos)
+        for pedido in self.pedidos:
+            if(pedido[0] == costumer):
+                produto.discard(pedido[1])
+        return produto
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        dias = set(self.semana)
+        for pedido in self.pedidos:
+            if(pedido[0] == costumer):
+                dias.discard(pedido[2])
+        return dias
 
     def get_busiest_day(self):
-        pass
+        semana = []
+        for pedido in self.pedidos:
+            semana.append(pedido[2])
+        return Counter(semana).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        semana = []
+        for pedido in self.pedidos:
+            semana.append(pedido[2])
+        return Counter(semana).most_common()[-1][0]
