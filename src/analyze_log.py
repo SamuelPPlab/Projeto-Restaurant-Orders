d91c5b6aa1ea1af28a6ce049c08415c3894151ef
lisvalver <lisvalver.atte@gmail.com>
2021-08-15 18:44:55 -0300

    Req 02 - Track orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,43 +1,43 @@
 import csv
 
 
-def most_requested_dish(orders_csv, client):
+def most_requested_dish(orders_csv, costumer):
     orders = {}
-    for client_name, client_dish, day in orders_csv:
-        if client_name == client:
-            if client_dish in orders_csv:
-                orders[client_dish] += 1
+    for costumer_name, costumer_dish, day in orders_csv:
+        if costumer_name == costumer:
+            if costumer_dish in orders_csv:
+                orders[costumer_dish] += 1
             else:
-                orders[client_dish] = 1
+                orders[costumer_dish] = 1
     return max(orders, key=orders.get)
 
 
-def count_client_dish(orders_csv, client, dish):
+def count_costumer_dish(orders_csv, costumer, dish):
     counter = 0
-    for client_name, client_dish, day in orders_csv:
-        if client_name == client and client_dish == dish:
+    for costumer_name, costumer_dish, day in orders_csv:
+        if costumer_name == costumer and costumer_dish == dish:
             counter += 1
     return counter
 
 
-def dishes_never_ordered_by_client(orders_csv, client):
+def dishes_never_ordered_by_costumer(orders_csv, costumer):
     all_dishes = set()
-    client_dishes = set()
-    for client_dish in orders_csv:
-        all_dishes.add(client_dish[1])
-        if client_dish[0] == client:
-            client_dishes.add(client_dish[1])
-    return all_dishes.difference(client_dishes)
+    costumer_dishes = set()
+    for costumer_dish in orders_csv:
+        all_dishes.add(costumer_dish[1])
+        if costumer_dish[0] == costumer:
+            costumer_dishes.add(costumer_dish[1])
+    return all_dishes.difference(costumer_dishes)
 
 
-def days_never_visited_by_client(orders_csv, client):
+def days_never_visited_by_costumer(orders_csv, costumer):
     all_days = set()
-    client_days = set()
-    for client_day in orders_csv:
-        all_days.add(client_day[2])
-        if client_day[0] == client:
-            client_days.add(client_day[2])
-    return all_days.difference(client_days)
+    costumer_days = set()
+    for costumer_day in orders_csv:
+        all_days.add(costumer_day[2])
+        if costumer_day[0] == costumer:
+            costumer_days.add(costumer_day[2])
+    return all_days.difference(costumer_days)
 
 
 def read_orders_csv(path_to_file):
@@ -50,9 +50,9 @@ def read_orders_csv(path_to_file):
 def analyze_log(path_to_file):
     file_read = read_orders_csv(path_to_file)
     most_requested = most_requested_dish(file_read, 'maria')
-    count_dish = count_client_dish(file_read, 'arnaldo', 'hamburguer')
-    dishes_never_ordered = dishes_never_ordered_by_client(file_read, 'joao')
-    days_never_visited = days_never_visited_by_client(file_read, 'joao')
+    count_dish = count_costumer_dish(file_read, 'arnaldo', 'hamburguer')
+    dishes_never_ordered = dishes_never_ordered_by_costumer(file_read, 'joao')
+    days_never_visited = days_never_visited_by_costumer(file_read, 'joao')
 
     campaign_log_file = open("data/mkt_campaign.txt", "w")
 
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,55 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders = {}
+        for order in self.orders:
+            if order['costumer'] == costumer:
+                if order['order'] in orders:
+                    orders[order['order']] += 1
+                else:
+                    orders[order['order']] = 1
+        return max(orders, key=orders.get)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        all_dishes = set()
+        client_dishes = set()
+        for order in self.orders:
+            all_dishes.add(order['order'])
+            if order['costumer'] == costumer:
+                client_dishes.add(order['order'])
+        return all_dishes.difference(client_dishes)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        all_days = set()
+        client_days = set()
+        for order in self.orders:
+            all_days.add(order['day'])
+            if order['costumer'] == costumer:
+                client_days.add(order['day'])
+        return all_days.difference(client_days)
 
     def get_busiest_day(self):
-        pass
+        daily_frequency = {}
+        for order in self.orders:
+            if order['day'] in daily_frequency:
+                daily_frequency[order['day']] += 1
+            else:
+                daily_frequency[order['day']] = 1
+        return max(daily_frequency, key=daily_frequency.get)
 
     def get_least_busy_day(self):
-        pass
+        daily_frequency = {}
+        for order in self.orders:
+            if order['day'] in daily_frequency:
+                daily_frequency[order['day']] += 1
+            else:
+                daily_frequency[order['day']] = 1
+        return min(daily_frequency, key=daily_frequency.get)
