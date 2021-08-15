b56e49cbfbc9251a0689527b7f0a2e97baf642cb
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-15 18:22:42 -0300

    feat: first try

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -24,35 +24,21 @@ def never_requested_dishes(orders, person):
 
 def never_attended_days(orders, person):
     days = set([order[2] for order in orders])
-    person_orders = set(
-            [order[2] for order in orders if order[0] == person]
-        )
-    return (days.difference(person_orders)).sort()
+    person_orders = set(list(map(lambda order: order[2], filter(
+        lambda name: name[0] == person, orders))))
+    return days.difference(person_orders)
 
 
 def analyze_log(path_to_file):
-    result_list = list()
-
     with open(path_to_file, mode="r") as file:
         orders = [row for row in csv.reader(file)]
-
         requested = most_requested_dish(orders, 'maria')
-        result_list.append(f"{requested}\n")
-
         times = how_many_times_was_requested(orders, 'arnaldo', 'hamburguer')
-        result_list.append(f"{times}\n")
-
         never_requested = never_requested_dishes(orders, 'joao')
-        result_list.append(f"{never_requested}\n")
-
         never_attended = never_attended_days(orders, 'joao')
-        result_list.append(f"{never_attended}\n")
 
     with open("./data/mkt_campaign.txt", mode="w") as file:
-        file.writelines(result_list)
-
-
-
-
-""" x = [['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'hamburguer', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira'], ['maria', 'hamburguer', 'terça-feira'], ['joao', 'hamburguer', 'terça-feira'], ['maria', 'pizza', 'terça-feira'], ['maria', 'coxinha', 'segunda-feira'], ['arnaldo', 'misto-quente', 'terça-feira'], ['jose', 'hamburguer', 'sabado'], ['maria', 'hamburguer', 'terça-feira']]
-print(never_attended_days(x, 'joao')) """
+        file.write(f'{requested}\n')
+        file.write(f'{str(times)}\n')
+        file.write(f'{str(never_requested)}\n')
+        file.write(f'{str(never_attended)}\n')
