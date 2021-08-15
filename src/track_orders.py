992ea0b8c4a8c3ca9ffb512c3659006371e3f770
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-15 20:35:49 -0300

    requisito 02-v6

--- src/track_orders.py
+++ src/track_orders.py
@@ -20,6 +20,13 @@ class TrackOrders:
         current_order = [costumer, order, day]
         self.order_list.append(current_order)
 
+    def filter_days_of_the_week_not_repeated_in_list(array):
+        new_list = []
+        for item in array:
+            if item[2] not in new_list:
+                new_list.append(item[2])
+        return new_list
+
     """
     referência: https://stackoverflow.com/questions/3594514/
     how-to-find-most-common-elements-of-a-list/44481414
@@ -41,7 +48,9 @@ class TrackOrders:
         return all_orders.difference(list_costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        list_costumer = self.list_by_costumer(costumer)
+        days = self.filter_days_of_the_week_not_repeated_in_list(list_costumer)
+        return list_costumer.difference(days)
 
     def get_busiest_day(self):
         pass
