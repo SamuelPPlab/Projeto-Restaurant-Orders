9fc744b349790ea0147157fe324b1217ee0eebbb
Kesley Lahoz <namielahoz@gmail.com>
2021-08-15 19:46:57 -0300

    track_orders 2.4 and 2.5

--- src/track_orders.py
+++ src/track_orders.py
@@ -8,11 +8,29 @@ class TrackOrders:
     def add_new_order(self, costumer, order, day):
         self.orders.append((costumer, order, day))
 
-    def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+    def get_most_ordered_dish_per_costumer(self, customer):
+        most_asked_by_client = {}
+        favorite_dish = ""
+        times_asked = 0
+        for (name, food, _) in self.orders:
+            if name == customer:
+                if food in most_asked_by_client:
+                    most_asked_by_client[food] += 1
+                else:
+                    most_asked_by_client[food] = 1
+                if most_asked_by_client[food] > times_asked:
+                    favorite_dish = food
+                    times_asked = most_asked_by_client[food]
+        return favorite_dish
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
+    def get_never_ordered_per_costumer(self, customer):
+        dishes_asked_by_client = set()
+        dishes = set()
+        for (name, food, _) in self.orders:
+            dishes.add(food)
+            if name == customer:
+                dishes_asked_by_client.add(food)
+        return dishes.difference(dishes_asked_by_client)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
