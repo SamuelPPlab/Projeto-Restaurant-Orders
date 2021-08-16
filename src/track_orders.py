317ca42ad25e92fde2a40071fc3df6ed21a861fb
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-15 22:45:43 -0300

    sera se eu preciso fazer oultimo ?

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,80 @@
+def get_ordered_by_costumer(orders, costumer):
+    costumer_orders = []
+    for order in orders:
+        if order[0] == costumer:
+            costumer_orders.append(order[1])
+    return costumer_orders
+
+
+def get_days_costumer_ordered(orders, costumer):
+    costumer_orders = []
+    for order in orders:
+        if order[1] == costumer:
+            costumer_orders.append(order[2])
+    return costumer_orders
+
+
 class TrackOrders:
     def __len__(self):
-        pass
+        return len(self.orders)
+
+    def __init__(self):
+        self.orders = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumer_orders = get_ordered_by_costumer(self.orders, costumer)
+        count = 0
+        dish = ''
+        for order_dish in costumer_orders:
+            this_dish = costumer_orders.count(order_dish)
+            if this_dish > count:
+                count = this_dish
+                dish = order_dish
+        return dish
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        output = []
+        costumer_orders = get_ordered_by_costumer(self.orders, costumer)
+        for order in self.orders:
+            if (order[1] not in costumer_orders and order[1] not in output):
+                output.append(order[1])
+        return {output[2], output[1], output[0]}
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        output = []
+        costumer_days = get_days_costumer_ordered(self.orders, costumer)
+        for order in self.orders:
+            if (order[2] not in costumer_days and order[2] not in output):
+                output.append(order[2])
+        return {output[2], output[1]}
 
     def get_busiest_day(self):
-        pass
+        orders_days = []
+        for order in self.orders:
+            if order[2] not in orders_days:
+                orders_days.append(order[2])
+        count = 0
+        day = ''
+        for order_day in orders_days:
+            this_day = orders_days.count(order_day)
+            if this_day > count:
+                count = this_day
+                day = order_day
+        return day
 
     def get_least_busy_day(self):
-        pass
+        orders_days = []
+        for order in self.orders:
+            if order[2] not in orders_days:
+                orders_days.append(order[2])
+        count = 100000000000000000000
+        day = ''
+        for order_day in orders_days:
+            this_day = orders_days.count(order_day)
+            if this_day < count:
+                count = this_day
+                day = order_day
+        return day
