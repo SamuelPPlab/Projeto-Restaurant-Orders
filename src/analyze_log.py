a697e1a839c1d8dec8140d203a3ec0e0c8e80252
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-08-15 23:41:34 -0300

    req: 01, 02

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,122 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import collections
+import csv
+
+
+def get_all_orders(file_path):
+    with open(file_path, 'r') as file:
+        # file_to_dict = csv.DictReader(file)  # method for give keys
+        # for row in file_to_dict:
+        #     row['k1_customer'], row['k2_ordered_dish'], row['k3_week_day']
+        keys = ['k1_customer', 'k2_ordered_dish', 'k3_week_day']
+        file_to_dict = csv.DictReader(file, fieldnames=keys)
+        all_orders = []
+        [all_orders.append(requests) for requests in file_to_dict]
+        # print('ALL ORDERS', all_orders)
+    return all_orders
+
+
+def most_ordered_dishes(customer, all_orders):
+    ordered_dishs = [
+        order['k2_ordered_dish']
+        for order in all_orders
+        if order['k1_customer'] == customer
+    ]
+    most_order_dish = collections.Counter(ordered_dishs).most_common(1)[0][0]
+    return most_order_dish
+
+
+def dish_quantity(customer, dish, all_orders):
+    all_dishs_customer = [
+        order['k2_ordered_dish']
+        for order in all_orders
+        if order['k1_customer'] == customer
+    ]
+    count_dish = collections.Counter(all_dishs_customer).get(dish)
+    return count_dish
+
+
+def never_ordered(customer, all_orders):
+    dishs_types = {
+        order['k2_ordered_dish']
+        for order in all_orders
+    }
+    ordered_dish_customer = {
+        order['k2_ordered_dish']
+        for order in all_orders
+        if order['k1_customer'] == customer
+    }
+
+    dish_never_ordered = dishs_types ^ ordered_dish_customer
+    return dish_never_ordered
+
+
+def days_never_visited(customer, all_orders):
+    get_all_days = {
+        week_day['k3_week_day']
+        for week_day in all_orders
+    }
+    days_in = {
+        week_day['k3_week_day']
+        for week_day in all_orders
+        if week_day['k1_customer'] == customer
+    }
+    days_out = get_all_days ^ days_in
+    return days_out
+
+
+def analyze_log(file_path):
+    if file_path.endswith('.csv'):
+        txt_file = 'data/mkt_campaign.txt'
+        all_orders = get_all_orders(file_path)
+        txt = [
+            f"{most_ordered_dishes('maria', all_orders)}\n",
+            f"{dish_quantity('arnaldo','hamburguer',all_orders)}\n",
+            f"{never_ordered('joao', all_orders)}\n",
+            f"{days_never_visited('joao', all_orders)}",
+        ]
+        with open(txt_file, 'w') as arquivo:
+            arquivo.writelines(txt)
+
+        # most_ordered_dishe = most_ordered_dishes('maria', all_orders)
+        # count_d_by_c = dish_quantity('arnaldo','hamburguer',all_orders)
+        # get_nonexistent_orders = never_ordered('joao', all_orders)
+        # days_not_in = days_never_visited('joao', all_orders)
+        # txt = [
+        #     f'{most_ordered_dishe}\n',
+        #     f'{count_d_by_c}\n',
+        #     f'{get_nonexistent_orders}\n',
+        #     f'{days_not_in}',
+        # ]
+        # with open(txt_file, 'w') as arquivo:
+        #     arquivo.writelines(txt)
+
+        # txt = []
+        # txt.append(most_ordered_dishes('maria', all_orders))
+        # txt.append(dish_quantity('arnaldo', 'hamburguer', all_orders))
+        # txt.append(never_ordered('joao', all_orders))
+        # txt.append(days_never_visited('joao', all_orders))
+        # with open(txt_file, 'w') as file:  # 05
+        #     for element in txt:
+        #         file.write(str(element) + "\n")
+    else:
+        raise FileNotFoundError(
+            f"No such file or directory: '{file_path}'"
+        )
+
+
+if __name__ == '__main__':
+    analyze_log('data/orders_1.csv')
+
+# python3 -m pytest tests/test_analyze_log.py
+# $ source .venv/bin/activate
+
+# referencias
+# Tive ajuda de ZecaCosta T6,T7
+# 01 - https://docs.python.org/3/library/csv.html
+# 02 - https://docs.python.org/3/library/collections.html
+# 03 - https://www.codegrepper.com/code-examples/python/python+collections...
+# 04 - https://stackoverflow.com/questions/50871370/python-sets-difference...
+# 04 - https://www.programiz.com/python-programming/methods/set/symmetric_...
+# 05 - https://stackoverflow.com/questions/37289951/how-to-write-to-a-csv-...
+# 06 - https://www.pythonforbeginners.com/files/reading-and-writing-files-...
+# https://www.geeksforgeeks.org/defaultdict-in-python/
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,79 @@
+import collections
+
+
 class TrackOrders:
+    def __init__(self):
+        self.all_orders = []
+
     def __len__(self):
-        pass
+        # pass
+        return len(self.all_orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        # pass
+        self.all_orders.append({
+            'k1_custumer': costumer,
+            'k2_ordered_dish': order,
+            'k3_week_day': day
+        })
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        # pass
+        ordered_dishs = [
+            order['k2_ordered_dish']
+            for order in self.all_orders
+            if order['k1_custumer'] == costumer
+        ]
+        mostorderdish = collections.Counter(ordered_dishs).most_common(1)[0][0]
+        return mostorderdish
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        # pass
+        dishs_types = {
+            order['k2_ordered_dish']
+            for order in self.all_orders
+        }
+        ordered_dish_customer = {
+            order['k2_ordered_dish']
+            for order in self.all_orders
+            if order['k1_custumer'] == costumer
+        }
+        dish_never_ordered = dishs_types ^ ordered_dish_customer
+        return dish_never_ordered
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        # pass
+        get_all_days = {
+            week_day['k3_week_day']
+            for week_day in self.all_orders
+        }
+        days_in = {
+            week_day['k3_week_day']
+            for week_day in self.all_orders
+            if week_day['k1_custumer'] == costumer
+        }
+        days_out = get_all_days ^ days_in
+        return days_out
 
     def get_busiest_day(self):
-        pass
+        # pass
+        days_in = [
+            week_day['k3_week_day']
+            for week_day in self.all_orders
+        ]
+        count_li = collections.Counter(days_in)
+        busiest_day = list(count_li.keys())[0]
+        return busiest_day
 
     def get_least_busy_day(self):
         pass
+        days_in = [
+            week_day['k3_week_day']
+            for week_day in self.all_orders
+        ]
+        count_li = collections.Counter(days_in)
+        busiest_day = list(count_li.keys())[-1]
+        return busiest_day
+
+# python3 -m pytest tests/test_track_orders.py
+# referencias
