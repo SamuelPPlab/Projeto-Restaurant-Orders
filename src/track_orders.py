8f4783b1530ba75d8eb9348c0f5f7ef6d4006acb
renatomak <renato.mark.silva@gmail.com>
2021-08-16 22:49:49 -0300

    Requisito 2 - tamanho do objeto e metodo de adicionar

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,11 +1,24 @@
+import csv
+
+
 class TrackOrders:
+    def __init__(self) -> None:
+        self.track_orders = []
+
     def __len__(self):
-        pass
+        return len(self.track_orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        with open('data/orders_1.csv', "a") as file:
+            writer = csv.writer(file)
+            row = list((costumer, order, day))
+            writer.writerow(row)
+            self.track_orders.append(row)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
+        pass
+
+    def get_dish_quantity_per_costumer(self, costumer, dish):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
@@ -19,3 +32,9 @@ class TrackOrders:
 
     def get_least_busy_day(self):
         pass
+
+
+if __name__ == "__main__":
+    client = TrackOrders()
+    client.add_new_order('renato', 'pizza', 'segunda')
+    print(len(client))
