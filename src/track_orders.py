479445aed55853265a5d0bd4a4f0ce9dcc51e03d
vitorAmorims <vtamorims@gmail.com>
2021-08-17 16:04:12 -0300

    correccao sintaxe flake

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,4 +1,4 @@
-from collections import Counter
+# from collections import Counter
 
 class TrackOrders:
     def __init__(self):
@@ -8,7 +8,7 @@ class TrackOrders:
         return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        return self.orders.append({'a':costumer, 'b':order, 'c':day})
+        return self.orders.append({'a': costumer, 'b': order, 'c': day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         mais_pedido = {}
@@ -22,7 +22,6 @@ class TrackOrders:
                 mais_pedido.items(), key=lambda kv: kv[1], reverse=True)
         return sorted_pedidos[0][0]
 
-    
     def get_dish_quantity_per_costumer(self, costumer, order):
         pass
 
