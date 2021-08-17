1a5ba91ede4ebc07fcfa0774edf039e1d12b14fe
renatomak <renato.mark.silva@gmail.com>
2021-08-16 23:00:07 -0300

    Requisito 2 - consultar prato mais pedido

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,7 +1,10 @@
 import csv
+from src.analyze_log import open_file, consult_most_frequent_order_by_client
 
 
 class TrackOrders:
+    path = 'data/orders_1.csv'
+
     def __init__(self) -> None:
         self.track_orders = []
 
@@ -9,14 +12,15 @@ class TrackOrders:
         return len(self.track_orders)
 
     def add_new_order(self, costumer, order, day):
-        with open('data/orders_1.csv', "a") as file:
+        with open(self.path, "a") as file:
             writer = csv.writer(file)
             row = list((costumer, order, day))
             writer.writerow(row)
             self.track_orders.append(row)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        data = open_file(self.path)
+        return consult_most_frequent_order_by_client(data, costumer)
 
     def get_dish_quantity_per_costumer(self, costumer, dish):
         pass
@@ -38,3 +42,4 @@ if __name__ == "__main__":
     client = TrackOrders()
     client.add_new_order('renato', 'pizza', 'segunda')
     print(len(client))
+    client.get_most_ordered_dish_per_costumer('maria')
