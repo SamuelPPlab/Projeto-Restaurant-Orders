2e864c696f1f87bed8c3698d1dd5ed6d35b6986e
renatomak <renato.mark.silva@gmail.com>
2021-08-17 11:03:47 -0300

    3 - Controle de estoque

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,10 +16,21 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.track_orders = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.track_orders.append(list((costumer, order, day)))
 
     def get_quantities_to_buy(self):
-        pass
+        used_ingredients = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0, }
+        for _, order, _ in self.track_orders:
+            for ingredient in self.INGREDIENTS[order]:
+                used_ingredients[ingredient] += 1
+        return used_ingredients
