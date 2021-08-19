5870f354d957833f2f40f352d4f5492e87f80558
vitorAmorims <vtamorims@gmail.com>
2021-08-19 14:39:56 -0300

    Req bonus oh gloria

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -36,45 +36,74 @@ class InventoryControl:
                 if i[0] == k and i[1] < v:
                     return False
 
-    # def inf_orders(self):
-    #     for i in self.orders:
-    #         if i['b'] in self.ingredients_orders:
-    #             self.ingredients_orders[i['b']] += 1
-    #         else:
-    #             self.ingredients_orders[i['b']] = 1
+    def inf_orders(self):
+        for i in self.orders:
+            if i['b'] in self.ingredients_orders:
+                self.ingredients_orders[i['b']] += 1
+            else:
+                self.ingredients_orders[i['b']] = 1
 
-    # def inf_ingredients(self):
-    #     self.inf_orders()
-    #     dict = {}
-    #     ingredientes = []
-    #     for k, v in self.ingredients_orders.items():
-    #         for index in self.ingredients:
-    #             if k == index[0]:
-    #                 ingredientes.append(index[1])
-    #     for value in ingredientes:
-    #         for value_i in value:
-    #             if value_i not in dict:
-    #                 dict[value_i] = 1
-    #             else:
-    #                 dict[value_i] += 1
-    #     return dict
+    def inf_ingredients(self):
+        self.inf_orders()
+        dict = {}
+        ingredientes = []
+        for k, v in self.ingredients_orders.items():
+            for index in self.ingredients:
+                if k == index[0]:
+                    ingredientes.append(index[1])
+        for value in ingredientes:
+            for value_i in value:
+                if value_i not in dict:
+                    dict[value_i] = 1
+                else:
+                    dict[value_i] += 1
+        return dict
 
-    # def inventario_x_consumo(self):
-    #     consumo = self.inf_ingredients()
-    #     print('consumo', consumo)
-    #     inventory = self.inventory
-    #     list_consumo = []
-    #     for k, v in consumo.items():
-    #         list_consumo.append((k, v))
-    #     list_inventory = []
-    #     for key, value in inventory[0].items():
-    #         list_inventory.append((key, value))
-    #     resp = []
-    #     for index in list_inventory:
-    #         for i in list_consumo:
-    #             if index[0] == i[0]:
-    #                 resp.append((index[0], index[1] - i[1]))
-    #     return resp
+    def consumo_de_ingredientes(self):
+        pedidos = self.pedidos()
+        dict = {}
+        for index in self.ingredients:#prato+ingredientes
+            # print(index[0]) # prato
+            # print(index[1]) # lista de ingredientes
+            for k, v in pedidos.items():
+                if k == index[0]:
+                    for i in index[1]:
+                        if i not in dict:
+                            dict[i] = v
+                        else:
+                            dict[i] += v
+        return dict
+        #         if index[0] == k:
+        #             for i in index[1]:
+        #                 if i not in dict:
+        #                     dict[i] = 1
+        #                 else:
+        #                     dict[i] += 1
+        # return dict
+
+        # consumo = self.inf_ingredients()
+        # inventory = self.inventory
+        # list_consumo = []
+        # for k, v in consumo.items():
+        #     list_consumo.append((k, v))
+        # list_inventory = []
+        # for key, value in inventory[0].items():
+        #     list_inventory.append((key, value))
+        # resp = []
+        # for index in list_inventory:
+        #     for i in list_consumo:
+        #         if index[0] == i[0]:
+        #             resp.append((index[0], index[1]))
+        # return resp
+    
+    def pedidos(self):
+        dict = {}
+        for i in self.orders:
+            if i['b'] not in dict:
+                dict[i['b']] = 1
+            else:
+                dict[i['b']] += 1
+        return dict
 
     def get_quantities_to_buy(self):
         pedidos = {}
@@ -98,9 +127,71 @@ class InventoryControl:
                         if value2 not in ingredientes:
                             ingredientes[value2] = 0
         return ingredientes
-    
+
     def get_available_dishes(self):
-        conj = set()
-        for i in self.ingredients:
-            conj.add((i[0]))
-        return conj
+        print(self.pedidos())
+        # for index in self.ingredients:#prato+ingredientes
+        # print(index[0]) #prato
+        # print(index[1]) # lista de ingredientes
+
+        #self.inventory[0] #quantidade de ingredientes minimo
+        # {'pao': 50, 'carne': 50, 'queijo': 100, 'molho': 50, 'presunto': 50, 'massa': 50, 'frango': 50}
+        # for k,v in self.inventory[0].items():
+        #     print(k)
+        #     print(v)
+
+        # consumo_ingredientes = self.consumo_de_ingredientes()
+        # for i in consumo_ingredientes:
+        #     print(i[0]) #ingrediente
+        #     print(i[1]) #quantidade
+            # massa
+            # 50
+            # frango
+            # 50
+
+        consumo_ingredientes = self.consumo_de_ingredientes()
+        print('consumo de ingredientes', consumo_ingredientes)
+        fingredientes = set()
+        for k, v in self.inventory[0].items():
+            for key, value in consumo_ingredientes.items():
+                # print('ingrediente', i[0])
+                # print('quantidade', i[1])
+                if k == key and v == value:
+                    fingredientes.add((k))
+        print('fingredientes', fingredientes)
+
+        idisponiveis = set()
+        for index in self.ingredients:#prato+ingredientes
+            # print(index[0]) #prato
+            # print(index[1]) # lista de ingredientes
+            for i in index[1]:
+                if i not in fingredientes:
+                    # print('sim', i)
+                    # sim carne
+                    # sim queijo
+                    # sim queijo
+                    # sim molho
+                    # sim pao
+                    # sim queijo
+                    # sim presunto
+                    idisponiveis.add((i))
+        print('idisponiveis', idisponiveis)
+
+        lista = set()
+        for index in self.ingredients:#prato+ingredientes
+            # print(index[0]) #prato
+            # print(index[1]) # lista de ingredientes
+            tamanho = len(index[1])
+            count = 0
+            for i in index[1]:
+                if i in idisponiveis:
+                    count += 1
+                    if count == tamanho:
+                        lista.add((index[0]))
+        print('pratos disponiveis', lista)
+
+        # conj = set()
+        # for i in self.ingredients:
+        #     conj.add((i[0]))
+        # return conj
+        return lista
