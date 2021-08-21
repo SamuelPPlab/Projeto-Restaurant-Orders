d6a74160f4c1f6f8f6639abfe05995a56f6d8fdb
Bruno Nogueira <nugobruno@gmail.com>
2021-08-21 03:27:23 -0300

    req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -5,10 +5,8 @@ class TrackOrders:
     def __len__(self):
         return len(self.orders)
 
-
     def add_new_order(self, costumer, order, day):
         self.orders.append({'costumer': costumer, 'order': order, 'day': day})
-
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         costumer_orders = {}
@@ -19,21 +17,20 @@ class TrackOrders:
                     costumer_orders[order['order']] = 1
                 else:
                     costumer_orders[order['order']] += 1
-        
+
         # # source:
         # # https://stackoverflow.com/questions/42044090
         # # /return-the-maximum-value-from-a-dictionary/42044202
         max_value = max(costumer_orders.values())
-        most_ordered_dish = [k for k, v in costumer_orders.items() if v == max_value]
+        most_ordered_dish = [
+            k for k, v in costumer_orders.items() if v == max_value
+        ]
 
         return most_ordered_dish[0]
-
 
     def get_never_ordered_per_costumer(self, costumer):
         all_orders = set()
         costumer_orders = set()
-
-        never_ordered = set()
 
         for order in self.orders:
             if order['order'] not in all_orders:
@@ -43,18 +40,11 @@ class TrackOrders:
                 if order['order'] not in costumer_orders:
                     costumer_orders.add(order['order'])
 
-        for order in all_orders:
-            if order not in costumer_orders:
-                never_ordered.add(order)
-
-        return never_ordered
-
+        return all_orders.difference(costumer_orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
         all_days = set()
         costumer_days = set()
-
-        never_visited = set()
 
         for order in self.orders:
             if order['day'] not in all_days:
@@ -64,12 +54,7 @@ class TrackOrders:
                 if order['day'] not in costumer_days:
                     costumer_days.add(order['day'])
 
-        for order in all_days:
-            if order not in costumer_days:
-                never_visited.add(order)
-
-        return never_visited
-
+        return all_days.difference(costumer_days)
 
     def get_busiest_day(self):
         days = {}
@@ -79,12 +64,11 @@ class TrackOrders:
                 days[order['day']] = 1
             else:
                 days[order['day']] += 1
-        
+
         max_value = max(days.values())
         busiest_day = [k for k, v in days.items() if v == max_value]
 
         return busiest_day[0]
-
 
     def get_least_busy_day(self):
         days = {}
@@ -94,7 +78,7 @@ class TrackOrders:
                 days[order['day']] = 1
             else:
                 days[order['day']] += 1
-        
+
         min_value = min(days.values())
         busiest_day = [k for k, v in days.items() if v == min_value]
 
