3272f7eed7c7a26395b24fc17c75c968d0c2260f
Lucas Andrade <lafdesouza2002@outlook.com>
2021-08-22 17:00:00 -0300

    Requisito 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,14 +17,26 @@ class InventoryControl:
 
     def __init__(self):
         self.orders = []
-        self.quantity_to_buy = self.MINIMUM_INVENTORY
+        self.quantity_to_buy = self.MINIMUM_INVENTORY.copy()
         for element in self.quantity_to_buy.keys():
             self.quantity_to_buy[element] = 0
 
     def add_new_order(self, costumer, order, day):
+        if order not in self.get_available_dishes():
+            return False
         self.orders.append({'client': costumer, 'order': order, 'day': day})
         for element in self.INGREDIENTS[order]:
             self.quantity_to_buy[element] += 1
 
     def get_quantities_to_buy(self):
         return self.quantity_to_buy
+
+    def get_available_dishes(self):
+        available = []
+        for key in self.INGREDIENTS.keys():
+            available.append(key)
+        for key, value in self.INGREDIENTS.items():
+            for ingredient in value:
+                if self.MINIMUM_INVENTORY[ingredient] - self.quantity_to_buy[ingredient] <= 0 and key in available:
+                    available.remove(key)
+        return set(available)
--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def print_info(tracker, control):
 
 def main():
     topic = 'order'
-    path = "C:/Users/lafde/sd-07-restaurant-orders/data/orders_1.csv"
+    path = "C:/Users/lafde/sd-07-restaurant-orders/data/orders_2.csv"
 
     tracker = TrackOrders()
     control = InventoryControl()
