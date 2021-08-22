e6b2db07357483fedaf38e63cb0c074931c3057e
Lucas Andrade <lafdesouza2002@outlook.com>
2021-08-22 13:19:57 -0300

    Corrigindo lint

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,7 @@
 import csv
 from collections import Counter
 
+
 def most_demanded_by_client(name, list_orders):
     orders = []
     for element in list_orders:
@@ -9,12 +10,14 @@ def most_demanded_by_client(name, list_orders):
     counter = Counter(orders)
     return list(counter.keys())[0]
 
+
 def quantity_order(name, order, list_orders):
     c = 0
     for element in list_orders:
         if element['client'] == name and element['order'] == order:
             c = c + 1
     return c
+
 
 def orders_never_ordered(name, list_orders):
     orders_already_ordered = set()
@@ -25,6 +28,7 @@ def orders_never_ordered(name, list_orders):
             orders_already_ordered.add(element['order'])
     return all_orders.difference(orders_already_ordered)
 
+
 def days_never_gone(name, list_orders):
     days_already_done = set()
     all_days = set()
@@ -33,6 +37,7 @@ def days_never_gone(name, list_orders):
         if element['client'] == name:
             days_already_done.add(element['day'])
     return all_days.difference(days_already_done)
+
 
 def analyze_log(path_to_file):
     if path_to_file.endswith('.csv'):
