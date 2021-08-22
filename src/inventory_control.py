583c6dac0079e1f71f7aa540515b49a39c68d548
Lucas Andrade <lafdesouza2002@outlook.com>
2021-08-22 14:55:54 -0300

    Requisito 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,10 +16,15 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.orders = []
+        self.quantity_to_buy = self.MINIMUM_INVENTORY
+        for element in self.quantity_to_buy.keys():
+            self.quantity_to_buy[element] = 0
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({'client': costumer, 'order': order, 'day': day})
+        for element in self.INGREDIENTS[order]:
+            self.quantity_to_buy[element] += 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.quantity_to_buy
