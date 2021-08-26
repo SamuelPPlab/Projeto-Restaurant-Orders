d1acff16f0e9dabe3414ac264d292e8106ef7a4d
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-26 18:40:21 -0300

    feat: implement inventory control class

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class InventoryControl:
     INGREDIENTS = {
         'hamburguer': ['pao', 'carne', 'queijo'],
@@ -16,10 +19,21 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.recipes = dict(self.INGREDIENTS)
+        self.inventory = dict(self.MINIMUM_INVENTORY)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        for item in self.recipes[order]:
+            if self.inventory[item] > 0:
+                self.inventory[item] -= 1
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        itens_to_buy = Counter(self.MINIMUM_INVENTORY) - Counter(
+            self.inventory
+        )
+        for i in itens_to_buy:
+            if itens_to_buy[i] == 0:
+                itens_to_buy.pop(i)
+        return itens_to_buy
