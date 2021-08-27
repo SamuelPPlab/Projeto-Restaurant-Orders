752d256a84d527bbcca5f33ec5f8c60c98a04977
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-27 11:52:24 -0300

    fix: get_quantities_to_buy keys: 0, get_available_dishes crash fix

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -22,18 +22,31 @@ class InventoryControl:
         self.recipes = dict(self.INGREDIENTS)
         self.inventory = dict(self.MINIMUM_INVENTORY)
 
+    def get_available_dishes(self):
+        recipes_to_remove = set()
+        for meal in self.recipes:
+            for ingredient in self.recipes[meal]:
+                if self.inventory[ingredient] == 0:
+                    recipes_to_remove.add(meal)
+        for meal in recipes_to_remove:
+            self.recipes.pop(meal)
+        return self.recipes.keys()
+
     def add_new_order(self, costumer, order, day):
-        for item in self.recipes[order]:
-            if self.inventory[item] > 0:
-                self.inventory[item] -= 1
-            else:
-                return False
+        self.get_available_dishes()
+        try:
+            for item in self.recipes[order]:
+                if self.inventory[item] > 0:
+                    self.inventory[item] -= 1
+        except(AssertionError):
+            return False
 
     def get_quantities_to_buy(self):
         itens_to_buy = Counter(self.MINIMUM_INVENTORY) - Counter(
             self.inventory
         )
-        for i in itens_to_buy:
-            if itens_to_buy[i] == 0:
-                itens_to_buy.pop(i)
+        for i in self.inventory:
+            if i not in itens_to_buy:
+                itens_to_buy.update({i: 0})
+
         return itens_to_buy
