0b1dff3393d8159f7017027d4222239ccd221905
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-28 16:29:55 -0300

    checking requirement 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,10 +1,12 @@
 class InventoryControl:
+
     INGREDIENTS = {
         'hamburguer': ['pao', 'carne', 'queijo'],
         'pizza': ['massa', 'queijo', 'molho'],
         'misto-quente': ['pao', 'queijo', 'presunto'],
         'coxinha': ['massa', 'frango'],
     }
+
     MINIMUM_INVENTORY = {
         'pao': 50,
         'carne': 50,
@@ -16,10 +18,24 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.orders = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_quantities_to_buy(self):
-        pass
+        ingredients_to_buy = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
+        for order in self.orders:
+            for ingredient in self.INGREDIENTS[order[1]]:
+                ingredients_to_buy[ingredient] += 1
+
+        return ingredients_to_buy
