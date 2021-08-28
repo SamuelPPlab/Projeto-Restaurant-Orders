37154bde18a8c55c2157c6206a25f5d44dd9b8a7
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-28 17:52:34 -0300

    checking requirement 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,27 +1,34 @@
 class InventoryControl:
 
-    INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
-    }
-
-    MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
-    }
-
     def __init__(self):
         self.orders = []
+        self.MINIMUM_INVENTORY = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
+        self.INGREDIENTS = {
+            'hamburguer': ['pao', 'carne', 'queijo'],
+            'pizza': ['massa', 'queijo', 'molho'],
+            'misto-quente': ['pao', 'queijo', 'presunto'],
+            'coxinha': ['massa', 'frango'],
+        }
 
     def add_new_order(self, costumer, order, day):
+        for ingredient in self.INGREDIENTS[order]:
+            if (self.MINIMUM_INVENTORY[ingredient] - 1) < 0:
+                return False
+
+        # set order
         self.orders.append((costumer, order, day))
+
+        # update inventory
+        for ingredient in self.INGREDIENTS[order]:
+            self.MINIMUM_INVENTORY[ingredient] -= 1
 
     def get_quantities_to_buy(self):
         ingredients_to_buy = {
@@ -39,3 +46,17 @@ class InventoryControl:
                 ingredients_to_buy[ingredient] += 1
 
         return ingredients_to_buy
+
+    def get_available_dishes(self):
+        available_dishes = set()
+        check = True
+        for dish in self.INGREDIENTS.keys():
+            for ingredients in self.INGREDIENTS[dish]:
+                if self.MINIMUM_INVENTORY[ingredients] <= 0:
+                    check = False
+                    break
+            if check:
+                available_dishes.add(dish)
+            check = True
+        print(self.MINIMUM_INVENTORY)
+        return available_dishes
