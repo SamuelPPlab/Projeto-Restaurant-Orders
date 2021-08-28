3a9b9d3d38eeeb813fef4f81135259682fc9005e
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-28 15:09:50 -0300

    optimization of requirement 2 using functions of requirement 1

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,6 @@
+import src.analyze_log as func 
+
+
 class TrackOrders:
     def __init__(self):
         self.orders = []
@@ -9,51 +12,13 @@ class TrackOrders:
         self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        plates_count = dict()
-        max_order_plate = 0
-        favorite_plate = ""
-        for order in self.orders:
-
-            # checking if client ordered is equal to costumer
-            if order[0] == costumer:
-
-                # checkint if plate are already on plates_count dict
-                if order[1] in plates_count:
-                    plates_count[order[1]] += 1
-                else:
-                    plates_count[order[1]] = 1
-
-                # checking if actual plate most requested
-                if plates_count[order[1]] > max_order_plate:
-                    max_order_plate = plates_count[order[1]]
-                    favorite_plate = order[1]
-        return favorite_plate
+        return func.favorite_plate(costumer, self.orders)
 
     def get_never_ordered_per_costumer(self, costumer):
-        order_client = set()
-        all_orders = set()
-
-        # order[1] = plate ordered
-        # order[0] = client that make order
-        for order in self.orders:
-            all_orders.add(order[1])
-        for order in self.orders:
-            if order[0] == costumer:
-                order_client.add(order[1])
-        return all_orders.difference(order_client)
+        return func.plates_never_ordered(costumer, self.orders)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        days_with_orders = set()
-        all_days = set()
-
-        # order[2] = day of order
-        # order[0] = client that make order
-        for order in self.orders:
-            all_days.add(order[2])
-        for order in self.orders:
-            if order[0] == costumer:
-                days_with_orders.add(order[2])
-        return all_days.difference(days_with_orders)
+        return func.days_without_orders(costumer, self.orders)
 
     def get_busiest_day(self):
         days_count_order = dict()
