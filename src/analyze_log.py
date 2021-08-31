516aa0c8604cf06f4cd679d8428ed51a5bf89caf
SamuelPPlab <samuelpp.s@hotmail.com>
2021-08-31 04:49:50 -0300

    funçao file importer criada

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,25 @@
+import csv
+
+
+def file_importer(path_to_file):
+    log = []
+    with open(path_to_file) as file:
+        csv_file = csv.reader(file)
+        for row in csv_file:
+            entry = {}
+            entry['cliente'] = row[0]
+            entry['pedido'] = row[1]
+            entry['dia'] = row[2]
+            log.append(entry)
+    return log
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    log = file_importer(path_to_file)
+    pedidos_de_maria = []
+    for item in log:
+        if (item['cliente'] == 'maria'):
+            pedidos_de_maria.append(item['pedido'])
+    pedidos_de_maria
+    print(pedidos_de_maria)
+
+analyze_log("data/orders_1.csv")
