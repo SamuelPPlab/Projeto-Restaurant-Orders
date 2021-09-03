cda333061063c563ebe4cdebd3268b7201547364
Leandro <leandro.feitoza@gmail.com>
2021-09-02 22:03:22 -0300

    ...

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,83 @@
+import csv
+
+
+def parse_csv(path_to_file):
+    with open(path_to_file) as file:
+        reader = csv.DictReader(file, ["Nome", "Pedido", "Dia"])
+        data = list(line for line in reader)
+    return data
+
+
+def get_orders_for_client(lista, name):
+    counter = {}
+    for item in lista:
+        if item["Nome"] == name:
+            if item["Pedido"] not in counter:
+                counter[item["Pedido"]] = 0
+            counter[item["Pedido"]] += 1
+    return counter
+
+
+def get_max_value_in_dict(dict_):
+    return max(zip(dict_.values(), dict_.keys()))[1]
+
+
+def get_quantity_order_by_client(lista, name, order):
+    quantity_order = 0
+    for item in lista:
+        if item["Nome"] == name and item["Pedido"] == order:
+            quantity_order += 1
+    return quantity_order
+
+
+def get_all_order_options(lista):
+    all_orders = []
+    for item in lista:
+        all_orders.append(item["Pedido"])
+    return set(all_orders)
+
+
+def get_all_days(lista):
+    all_days = []
+    for item in lista:
+        all_days.append(item["Dia"])
+    return set(all_days)
+
+
+def get_order_never_request(lista, name):
+    orders = []
+    for item in lista:
+        if item["Nome"] == name:
+            orders.append(item["Pedido"])
+    order_not_requested = set(orders)
+    all_menu = get_all_order_options(lista)
+    result = all_menu.difference(order_not_requested)
+    return result
+
+
+def get_days_not_visited(lista, name):
+    days = []
+    for item in lista:
+        if item["Nome"] == name:
+            days.append(item["Dia"])
+    days_visited = set(days)
+
+    all_days = get_all_days(lista)
+
+    return all_days.difference(days_visited)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as file:
+        reader = csv.DictReader(file, ["Nome", "Pedido", "Dia"])
+        data = list(line for line in reader)
+
+        report = [
+            f"{get_max_value_in_dict(get_orders_for_client(data, 'maria'))}\n"
+            f"{get_quantity_order_by_client(data, 'arnaldo', 'hamburguer')}\n"
+            f"{get_order_never_request(data, 'joao')}\n"
+            f"{get_days_not_visited(data, 'joao')}"
+        ]
+
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.writelines(report)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,43 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        most_ordered = []
+        for order in self.orders:
+            if order[0] == costumer:
+                most_ordered.append(order[1])
+        return Counter(most_ordered).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        foods = set(food[1] for food in self.orders)
+        foods_orderes_by_client = set(
+            item[1] for item in self.orders if item[0] == costumer
+        )
+
+        return foods - foods_orderes_by_client
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set(day[2] for day in self.orders)
+        days_visited = set(
+            item[2] for item in self.orders if item[0] == costumer
+        )
+
+        return days - days_visited
 
     def get_busiest_day(self):
-        pass
+        moviment_days = [day[2] for day in self.orders]
+        return Counter(moviment_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        moviment_days = [day[2] for day in self.orders]
+        return Counter(moviment_days).most_common()[-1][0]
