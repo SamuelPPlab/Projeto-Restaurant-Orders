579b53db13abadf6523ea7016b7ce2008882f2d3
Leandro <leandro.feitoza@gmail.com>
2021-09-02 23:32:45 -0300

    req 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,31 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.inventory = self.MINIMUM_INVENTORY.copy()
 
     def add_new_order(self, costumer, order, day):
-        pass
+        for item in self.INGREDIENTS[order]:
+            self.inventory[item] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        buy_list = dict()
+
+        for item in self.inventory:
+            total = self.MINIMUM_INVENTORY[item] - self.inventory[item]
+            buy_list[item] = total
+        return buy_list
