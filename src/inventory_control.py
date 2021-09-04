d6f41cd4b84f0189a9050caec75baf4780e62e00
josielvs <josielvs@gmail.com>
2021-09-03 22:51:49 -0300

    Requisito 3 e 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class InventoryControl:
     INGREDIENTS = {
         'hamburguer': ['pao', 'carne', 'queijo'],
@@ -16,10 +19,34 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.recipes = dict(self.INGREDIENTS)
+        self.inventory = dict(self.MINIMUM_INVENTORY)
+
+    def get_available_dishes(self):
+        recipes_remove = set()
+        for meal in self.recipes:
+            for ingredient in self.recipes[meal]:
+                if self.inventory[ingredient] == 0:
+                    recipes_remove.add(meal)
+        for meal in recipes_remove:
+            self.recipes.pop(meal)
+        return self.recipes.keys()
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.get_available_dishes()
+        try:
+            for item in self.recipes[order]:
+                if self.inventory[item] > 0:
+                    self.inventory[item] -= 1
+        except(KeyError):
+            return False
 
     def get_quantities_to_buy(self):
-        pass
+        itens_buy = Counter(self.MINIMUM_INVENTORY) - Counter(
+            self.inventory
+        )
+        for item in self.inventory:
+            if item not in itens_buy:
+                itens_buy.update({item: 0})
+
+        return itens_buy
